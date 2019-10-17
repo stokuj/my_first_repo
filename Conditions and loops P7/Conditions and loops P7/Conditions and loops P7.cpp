1e77d9743e620f5caccac2bb58a4e90c77442c7b
stokuj <stokujmen@gmail.com>
2019-10-17 23:53:14 +0200

    Upload p6-9

---
+++ Conditions and loops P7/Conditions and loops P7/Conditions and loops P7.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+
+
+int main()
+{
+	int N = 8;
+	int W = 5;
+	int K = 8;
+
+	char x = 'X';
+
+	for (int i = 0; i < N; i++)
+	{
+
+
+
+
+
+		for (int j = 0; j < N; j++)
+		{
+
+			std::cout << x;
+			if (x == 'X')
+			{
+
+				x = '.';
+			}
+			else
+			{
+				x = 'X';
+			}
+		}
+		
+		if (x == 'X')
+		{
+
+			x = '.';
+		}
+		else
+		{
+			x = 'X';
+		}
+		std::cout << std::endl;
+
+
+	}
+
+
+}
---
+++ Conditions and loops P8/Conditions and loops P8/Conditions and loops P8.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+
+
+int main()
+{
+	int N = 6;
+	int W = 5;
+	int K = 8;
+
+	char x = 'X';
+	char z = '.';
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N/2; j++)
+		{
+
+			std::cout << x;
+		}
+
+		for (int j = 0; j < N/2; j++)
+		{
+
+			std::cout << z;
+		}
+		if (x == 'X')
+		{
+
+			x = '.';
+			z = 'X';
+		}
+		else
+		{
+			z = '.';
+			x = 'X';
+		}
+
+		std::cout << std::endl;
+	}
+
+
+}
