5afcb18a16b8fcecdf2465d3432777d57e1c9187
stokuj <stokujmen@gmail.com>
2019-10-22 17:06:33 +0200

    complete program 6

--- Conditions and loops/Conditions and loops P6/Conditions and loops P6.cpp
+++ Conditions and loops/Conditions and loops P6/Conditions and loops P6.cpp
@@ -3,26 +3,36 @@
 
 int main()
 {
-	int N = 10;
-
-//    std::cout << "Enter N: ";
-//    std::cin >> N ;
+    std::cout << "Enter N: ";
+    int N;
+    std::cin >> N ;
 
 	char x = 'X';
 	char z = '.';
 
-	int num_x = 5;
-	int num_z = 4;
+    int num_0 = 0;
+	int num_1 = N/2; //  n/2
+	int num_2 = (N/2)-1; // (n/2)-1
+	int num_3 = (N/2) - ((N/2)-1); // (n/2) - ((n/2)-1)
 
-	for (int i = 0; i < N; i++)
+
+	for (int i = 0; i < N/2; i++)
 	{
 
 
 	    for (int l = 0; l < N;)
         {
+            for ( int j = 0; j < num_0; j++)
+            {
+                if (l == N)
+                {
+                    break;
+                }
+                std::cout << z;
+                l++;
+            }
 
-
-            for ( int j = 0; j < num_x; j++)
+            for ( int j = 0; j < num_1; j++)
             {
                 if (l == N)
                 {
@@ -33,7 +43,7 @@ int main()
             }
 
 
-            for ( int k = 0; k < num_z; k++)
+            for ( int j = 0; j < num_2; j++)
             {
                 if (l == N)
                 {
@@ -43,13 +53,88 @@ int main()
                 l++;
             }
 
+            for ( int j = 0; j < num_3; j++)
+            {
+                if (l == N)
+                {
+                    break;
+                }
+                std::cout << x;
+                l++;
+            }
+
+            num_0 ++;
+            num_1 --; //  n/2
+            num_2 --; // (n/2)-1
+            num_3 ++;
 
 
         }
 
+        std::cout << std::endl;
+	}
 
-		std::cout << std::endl;
+    num_0 = (N/2) ;
+    num_1 = 0 ;
+    num_2 = (N/2) - ((N/2)-1);
+    num_3 = (N/2)-1 ;
 
+    for (int i = 0; i < N/2; i++)
+    {
+
+
+	    for (int l = 0; l < N;)
+        {
+            for ( int j = 0; j < num_0; j++)
+            {
+                if (l == N)
+                {
+                    break;
+                }
+                std::cout << x;
+                l++;
+            }
+
+            for ( int j = 0; j < num_1; j++)
+            {
+                if (l == N)
+                {
+                    break;
+                }
+                std::cout << z;
+                l++;
+            }
+
+
+            for ( int j = 0; j < num_2; j++)
+            {
+                if (l == N)
+                {
+                    break;
+                }
+                std::cout <<x;
+                l++;
+            }
+
+            for ( int j = 0; j < num_3; j++)
+            {
+                if (l == N)
+                {
+                    break;
+                }
+                std::cout << z;
+                l++;
+            }
+
+            num_0 --;
+            num_1 ++; //  n/2
+            num_2 ++; // (n/2)-1
+            num_3 --;
+
+
+        }
+
+        std::cout << std::endl;
 	}
 
 }
